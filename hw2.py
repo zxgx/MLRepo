7c924a9cdb9a8a51fa7bb04384a6db166e0b9209
zxgx <zhangg1998@outlook.com>
2019-03-19 21:04:43 +0800

    deprecate adagrad, get worse performance

--- hw2.py
+++ hw2.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pylab as plt
 
 # Preprocess
-x_train = pd.read_csv(r'/home/zhg/share/hw2/X_train').values
-y_train = pd.read_csv(r'/home/zhg/share/hw2/Y_train').values
-x_test = pd.read_csv(r'/home/zhg/share/hw2/X_test').values
+x_train = pd.read_csv(r'D:\Temp\vshare\hw2\X_train').values
+y_train = pd.read_csv(r'D:\Temp\vshare\hw2\Y_train').values
+x_test = pd.read_csv(r'D:\Temp\vshare\hw2\X_test').values
 
 # normalization
 xmin = np.min(x_train, axis=0)
@@ -15,9 +16,10 @@ x_train = (x_train - xmin) / dif
 # Parameters Configuration
 W, b = np.random.rand(x_train.shape[1], 1), np.random.rand()
 epochs = 20
-batch_size=64
+batch_size = 64
 eta = 1
-ww, bb = np.zeros((x_train.shape[1], 1)), np.zeros(1)
+L, A = [], []
+step = 1
 
 # Training
 for i in range(epochs):
@@ -29,27 +31,37 @@ for i in range(epochs):
         y = np.clip(1 / (1 + np.exp(-z)), 1e-6, 1-1e-6);
 
         loss = np.sum(-1 * (Y * np.log(y) + (1 - Y) * np.log(1 - y)))
-        print('loss: ', loss)
+        L.append(loss)
 
-        dldw = np.sum((Y - y) * X, axis=0)[: ,np.newaxis]/X.shape[0]
-        dldb = np.sum(Y - y) / X.shape[0]
-        ww = ww + dldw ** 2
-        bb = bb + dldb ** 2
+        dldw = np.mean((y - Y) * X, axis=0)[: ,np.newaxis]
+        dldb = np.mean(y - Y)
 
-        W = W - eta / np.sqrt(ww) * dldw
-        b = b - eta / np.sqrt(bb) * dldb
+        W = W - eta / np.sqrt(step) * dldw
+        b = b - eta / np.sqrt(step) * dldb
+        step = step + 1
+
+    z = x_train.dot(W) + b
+    y = np.clip(1 / (1 + np.exp(-1 * z)), 1e-6, 1-1e-6)
+    y = np.round(y)
+    A.append(np.sum(y == y_train) / y.shape[0])
 
 z = x_train.dot(W) + b
 y = np.clip(1 / (1 + np.exp(-1 * z)), 1e-6, 1-1e-6)
 y = np.round(y)
 accuracy = np.sum(y == y_train) / y.shape[0]
 print('accuracy on training data: ', accuracy)
+plt.subplot(121)
+plt.plot(L)
+plt.subplot(122)
+plt.plot(A)
+plt.show()
 
+"""
 x_test = (x_test - xmin) / dif # There exists a bug that I don't want to fix 
 z = x_test.dot(W) + b
 y = np.clip(1 / (1 + np.exp(-1 * z)), 1e-6, 1-1e-6)
 y = np.round(y)
-"""
+
 d1 = np.array([x for x in range(1, 16282)]).reshape(16281, 1)
 d2 = np.array(y.astype(int))
 d = np.hstack((d1, d2))

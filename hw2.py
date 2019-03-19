b7d8da624a8294d51e2f01e78c4fa7ce562c4e5f
nebular <zhangg1998@outlook.com>
2019-03-19 20:37:44 +0800

    debug hw2

--- hw2.py
+++ hw2.py
@@ -14,50 +14,45 @@ x_train = (x_train - xmin) / dif
 
 # Parameters Configuration
 W, b = np.random.rand(x_train.shape[1], 1), np.random.rand()
-iteration = 200
+epochs = 20
+batch_size=64
 eta = 1
 ww, bb = np.zeros((x_train.shape[1], 1)), np.zeros(1)
-"""
-batch = 100
-if x_train.shape[0] % batch == 0:
-    batchs = x_train.shape[0] // batch
-else:
-    batchs = x_train.shape[0] // batch + 1
-"""
-# Loss = []
+
 # Training
-for i in range(iteration):
-    z = x_train.dot(W) + b
-    y = 1 / (1 + np.exp(-1 * z))
+for i in range(epochs):
+    for batch in range(int(np.floor(x_train.shape[0]/batch_size))):
+        X = x_train[batch*batch_size:(batch+1)*batch_size]
+        Y = y_train[batch*batch_size:(batch+1)*batch_size]
 
-    loss = np.sum(-1 * (y_train * np.log(y) + (1 - y_train) * np.log(1 - y)))
-    print('loss: ', loss)
-    # Loss.append(loss)
+        z = X.dot(W) + b
+        y = np.clip(1 / (1 + np.exp(-z)), 1e-6, 1-1e-6);
 
-    dldw = np.sum((y - y_train) * x_train, axis=0)[: ,np.newaxis]
-    dldb = np.sum(y - y_train)
-    ww = ww + dldw ** 2
-    bb = bb + dldb ** 2
+        loss = np.sum(-1 * (Y * np.log(y) + (1 - Y) * np.log(1 - y)))
+        print('loss: ', loss)
 
-    W = W - eta / np.sqrt(ww) * dldw
-    b = b - eta / np.sqrt(bb) * dldb
+        dldw = np.sum((Y - y) * X, axis=0)[: ,np.newaxis]/X.shape[0]
+        dldb = np.sum(Y - y) / X.shape[0]
+        ww = ww + dldw ** 2
+        bb = bb + dldb ** 2
+
+        W = W - eta / np.sqrt(ww) * dldw
+        b = b - eta / np.sqrt(bb) * dldb
 
 z = x_train.dot(W) + b
-y = 1 / (1 + np.exp(-1 * z))
-y[y < 0.5] = 0
-y[y >= 0.5] = 1
+y = np.clip(1 / (1 + np.exp(-1 * z)), 1e-6, 1-1e-6)
+y = np.round(y)
 accuracy = np.sum(y == y_train) / y.shape[0]
 print('accuracy on training data: ', accuracy)
 
 x_test = (x_test - xmin) / dif # There exists a bug that I don't want to fix 
 z = x_test.dot(W) + b
-y = 1 / (1 + np.exp(-1 * z))
-y[y < 0.5] = 0
-y[y >= 0.5] = 1
-
+y = np.clip(1 / (1 + np.exp(-1 * z)), 1e-6, 1-1e-6)
+y = np.round(y)
+"""
 d1 = np.array([x for x in range(1, 16282)]).reshape(16281, 1)
 d2 = np.array(y.astype(int))
 d = np.hstack((d1, d2))
 df = pd.DataFrame(d, columns=['id', 'label'])
 df.to_csv(r'/home/zhg/share/submission.csv', index=False)
-
+"""
